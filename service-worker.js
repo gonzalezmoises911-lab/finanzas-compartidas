ca34f4a762664f70da5625e608ae629fa07abe1f
gonzalezmoises911-lab <gonzalez.moises911@gmail.com>
2026-08-09 15:50:29 -0600

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,13 @@
-const CACHE_NAME = "finanzas-compartidas-v6";
-const APP_SHELL = ["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icon-192.png","./icon-512.png"];
+const CACHE_NAME = "finanzas-compartidas-v7";
+const APP_SHELL = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.webmanifest",
+  "./icon-192.png",
+  "./icon-512.png"
+];
 
 self.addEventListener("install", event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
@@ -14,11 +22,14 @@ self.addEventListener("activate", event => {
 });
 
 self.addEventListener("message", event => {
-  if (event.data && event.data.type === "SKIP_WAITING") self.skipWaiting();
+  if (event.data && event.data.type === "SKIP_WAITING") {
+    self.skipWaiting();
+  }
 });
 
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
+
   const url = new URL(event.request.url);
   if (url.origin !== self.location.origin) return;
 
@@ -38,13 +49,14 @@ self.addEventListener("fetch", event => {
         })
         .catch(() => caches.match(event.request).then(cached => cached || caches.match("./index.html")))
     );
-  } else {
-    event.respondWith(
-      caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-        const copy = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
-        return response;
-      }))
-    );
+    return;
   }
+
+  event.respondWith(
+    caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+      const copy = response.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(event.request, copy));
+      return response;
+    }))
+  );
 });
